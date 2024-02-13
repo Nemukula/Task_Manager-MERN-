c3a1451b08c534855a39eb865e45933c1f28226a
NAdi00 <nemukuladi000@gmail.com>
2024-02-13 12:29:55 +0200

    Update App.js

--- Task_Manager/sec/src/App.js
+++ Task_Manager/sec/src/App.js
@@ -4,55 +4,54 @@ import Header from './modules/Header';
 import Tasks from './modules/Tasks';
 import axios from 'axios';
 
-var global_data = [];
+coms
 
 function App() {
-
   //////Fecting data from backend,  node.js <- Mongodb
-  useEffect(() => {
-    const fetchData = async () => {
+  useEffect((data_main) => {
+    const fetchData = async (data_main) => {
         try {
             const response = await fetch('http://localhost:5051/read');
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             const data = await response.json();
-            global_data = data;
+            for (const i of data) {
+              global_data.push(i); 
+            }
+            setTasks(data)
+            console.log(global_data, 'global_data1');
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     };
+    
     fetchData();
 }, []);
+console.log(global_data, 'global_data4');
 
+const [state, setTasks] = useState (global_data);
 /////Initial state to be used when app is opened
-const [AA, setTasks] = useState (global_data);
-
 
 ///////////ADDING NEW TASK FUNCTION///////////////////////
 function handleAddTask()  {
     var input = document.getElementById('inputs');
-    let newId = Math.max(...AA.map(task => task.id), 0) + 1;
+    let newId = Math.max(...state.map(task => task.id), 0) + 1;
     let day = new Date().toLocaleDateString();
     let tesk = input.value;
-    let BB = [...AA, {id:`${newId}`, text: tesk, day: `${day}`}];
+    let BB = [...state, {id:`${newId}`, text: tesk, day: `${day}`}];
     setTasks([...BB]);
-  }
-
+}
   //////////////DELETING TASK FUNCTION///////////////////////
 function DeleteTask(my_input) {
-  setTasks(AA.filter((el)=> el.id !== my_input))
+  setTasks(state.filter((el)=> el.id !== my_input));
 }
 
 ////////////////SENDING DATA FROM REACT TO BACKEND(node.js -> mongodb)
 function sending(event) {
-    const dataToSend = {
-      key: 'value',
-      anotherKey: 'anotherValue'
-    };
 
     event.preventDefault();
-    axios.post('http://localhost:5051/fromFront', {dataToSend})
+    axios.post('http://localhost:5051/fromFront', {state})//Sending data from current tasks/state (setted state)
       .then(response => {
         console.log(response.data)
       })
@@ -64,10 +63,9 @@ function sending(event) {
   return (
     <div className="App">
       <Header title='TASK MANAGER' onAdding={handleAddTask}/><br />
-      <Tasks  title= {global_data}  onDelete = {DeleteTask}/>
+      <Tasks  title= {state}  onDelete = {DeleteTask}/>
       <button onClick={sending}>Save</button>
       </div>
   );
 }
-
 export default App;
